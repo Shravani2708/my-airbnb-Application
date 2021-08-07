869bac57c6c94373a69a9589a665f357c0f758cb
Shravani Badadha <sbadadh@calstatela.edu>
2021-08-06 22:11:20 -0700

    Day 2 build

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,71 @@
 import Image from "next/image";
+import { useRouter } from 'next/router';
+import Search from '../pages/Search';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+//Functional Component
+import { DateRangePicker } from 'react-date-range';
+
 import { SearchIcon,
         GlobeAltIcon,
         MenuIcon,
         UserCircleIcon,
-        UserIcon,
+        UsersIcon,
      } from "@heroicons/react/solid";
-
+import {useState} from "react";
+import { Router } from "next/dist/client/router";
 function Header() {
+//useState is a new way to use the exact same capabilities that this.state provides in a class
+//It returns a pair of values: the current state(searchInput) and a function(setSearchInput )that updates it
+    const [searchInput,setSearchInput]= useState("");
+    const[startDate,setStartDate]=useState(new Date());
+    const[endDate,setEndDate]=useState(new Date());
+    const[noOfGuests,setnoOfGuests]=useState(1);
+   
+    //configuration for datarange
+    const selectionRange={
+        startDate:startDate,
+        endDate:endDate,
+        key: "selection"
+
+    };
+
+    //for cancel button
+    const resetInput = () =>{
+        setSearchInput("");
+    };
+
+    //configuration to select start date and end date
+    const handleSelect= (ranges) =>{
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    };
+
+    const router = useRouter();
+    const search=()=>
+    {
+        router.push({
+            pathname:'/search',
+            query:{
+                location:searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                noOfGuests,
+
+            },
+
+        });
+            
+    };
+  
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5
         md:px-10">
              {/* left-image */}
-            <div className="relative flex items-center h-10
+            <div 
+            onClick={()=> router.push("/")}
+            className="relative flex items-center h-10
             cursor-pointer my-auto">
             
                 <Image src="https://links.papareact.com/qd3" 
@@ -23,7 +77,8 @@ function Header() {
             
             {/* middle-search */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600" type="text"  placeholder="Start your Search" />
+                <input value={searchInput}
+                onChange={(e)=> setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600" type="text"  placeholder="Start your Search" />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2
                 cursor-pointer md:mx-2"/>
                 
@@ -38,6 +93,36 @@ function Header() {
                 <UserCircleIcon className="h-6"/> 
             </div>
             </div>
+            {/*only if search input as value then only render hello
+            <h1>Hello</h1>*/}
+            {/*{searchInput && <h1>Hello</h1>}*/}
+            {searchInput && (
+            <div className= "flex flex-col col-span-3 mx-auto mt-3">
+                <DateRangePicker ranges = {[selectionRange]} 
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect}/>
+          
+            <div className="flex items-center border-b mb-4">
+                <h2 className="text-2xl flex-grow font-semibold"> Number of Guests</h2>
+                <UsersIcon className="h-5" />
+                <input
+                 value={noOfGuests}
+                 onChange={(e)=> setnoOfGuests(e.target.value)}
+                 
+                 type="number" min ='1' className="w-12 pl-2 text-lg items-center
+                outline-none text-red-400" />
+            </div>
+            <div>
+            <div className="flex">
+                <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+                <button  onClick={search}className="flex-grow text-red-400">Search</button>
+            </div>
+            </div>
+            </div>
+            
+            )}
+
             
         </header>
     );
